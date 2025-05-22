f02f2e15f611de3953ed00a507e2040e11d0e5cd
Molly Chi(紀岱欣) <Molly_Chi@asus.com>
2025-05-22 14:38:19 +0800

    test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://momolly1024.github.io/cs-master-note',
+  url: 'https://momolly1024.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/cs-master-note',
---
+++ note.md
@@ -0,0 +1,16 @@
+# build page
+
+## 
+
+after update/edit
+
+```code
+git add .
+git commit -m 'xxx'
+git push
+cmd /C "set "GIT_USER=momolly1024" && npm run deploy"
+```
+
+## PAGES
+`https://momolly1024.github.io/cs-master-note/`
+
